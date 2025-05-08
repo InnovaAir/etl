4add3d922709d6fa98f47cd76cc918a4ef7fd8bc
lucaspaessptech <lucas.paes@sptech.school>
2025-05-08 20:46:47 -0300

    ETL funcional e testando

--- etl-innovaair/src/main/java/innovaetl/Ordenar.java
+++ etl-innovaair/src/main/java/innovaetl/Ordenar.java
@@ -7,6 +7,15 @@ import java.util.Objects;
 public class Ordenar {
     public static List<Dado> ordernarPorValorCapturadoPorMetrica(List<Dado> dados){
         for (int i = 0; i < dados.size(); i ++){
+            for (int j = i+1; j < dados.size(); j++){
+                if (dados.get(j).getMetrica().compareTo(dados.get(i).getMetrica()) < 0){
+                    Dado temp = dados.get(i);
+                    dados.set(i, dados.get(j));
+                    dados.set(j, temp);
+                }
+            }
+        }
+        for (int i = 0; i < dados.size(); i ++){
             String metricaAtual = dados.get(i).getMetrica();
             for (int j = i+1; j < dados.size(); j++){
             String metrica = dados.get(j).getMetrica();
