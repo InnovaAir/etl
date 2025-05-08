66d548cde459d045649dd4f6d73a21f33be4c445
lucaspaessptech <lucas.paes@sptech.school>
2025-05-08 15:16:39 -0300

    ETL ajustada mas não testada

--- etl-innovaair/src/main/java/innovaetl/Main.java
+++ etl-innovaair/src/main/java/innovaetl/Main.java
@@ -25,6 +25,7 @@ public class Main implements RequestHandler<S3Event, String> {
                 InputStream s3InputStream = s3Client.getObject(sourceBucket, sourceKey).getObjectContent();
                 List<Dado> dados = Mapper.map(s3InputStream);
                 dados = Ordenar.ordernarPorValorCapturadoPorMetrica(dados);
+                dados = TratarValorCapturado.tratarParaMB(dados);
                 InputStream csvInput = Mapper.demap(dados);
                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
                 byte[] buffer = new byte[1024];
--- etl-innovaair/src/main/java/innovaetl/TratarValorCapturado.java
+++ etl-innovaair/src/main/java/innovaetl/TratarValorCapturado.java
@@ -1,4 +1,14 @@
 package innovaetl;
+import java.util.List;
 
 public class TratarValorCapturado {
+    public static List<Dado> tratarParaMB(List<Dado> dados) {
+        for (Dado dado:dados){
+            if(dado.getMetrica().equals("velocidadeDownload") || dado.getMetrica().equals("velocidadeUpload")){
+                dado.setValorCapturado(Math.round(dado.getValorCapturado()/1024**2));
+            }
+        }
+        return dados;
+    }
+
 }
