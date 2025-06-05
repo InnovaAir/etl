08b1194866fb7d55f02eceaaf299bccee2059fb0
leticia-rsm <leticia.mendes@sptech.school>
2025-06-05 12:34:42 -0300

    Alteração scprit para captura de dados externos

--- web-crawler/lambda_function.py
+++ web-crawler/lambda_function.py
@@ -24,10 +24,7 @@ def lambda_handler(event, context):
     ano_atual = datetime.now().year
     data = f"{ano_atual}{mes_atual:02d}"
 
-    url = f"https://olinda.bcb.gov.br/olinda/servico/Pix_DadosAbertos/versao/v1/odata/EstatisticasTransacoesPix(Database=@Database)?@Database='{data}'&$top=1000&$filter=FINALIDADE%20eq%20'Pix'%20and%20AnoMes%20eq%20{data}&$format=text/csv&$select=AnoMes,VALOR,QUANTIDADE"   
-
     try:
-
         start = datetime(2020, 1, 1)
         end = datetime(2020, 12, 31)
 
@@ -36,15 +33,15 @@ def lambda_handler(event, context):
         data = Daily(location, start, end)
         data = data.fetch()
 
-        print(data.head())
+        data = data.reset_index()
 
-        toCsv = data.to_csv('/tmp/clima.csv')
+        dadosFiltrados = data[['time', 'tavg', 'tmin', 'tmax', 'prcp', 'wspd']]
 
-        nome_arquivo = f"/tmp/pix{data}.csv"
-        with open(nome_arquivo, 'w', newline='') as csvfile:
-            csvfile.write(toCsv)
+        dadosFiltrados.to_csv('/tmp/clima.csv', index=False, encoding='utf-8-sig')
 
-        nome_arquivoDestino = f"pix/pix{data}.csv"
+        nome_arquivo = f"/tmp/clima{data}.csv"
+
+        nome_arquivoDestino = f"clima/clima{data}.csv"
         s3 = boto3.client('s3')
         s3.upload_file(nome_arquivo, 'bucket', nome_arquivoDestino)
 
