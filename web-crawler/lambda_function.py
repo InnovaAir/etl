d3795410bb9f77c2af9e7a879edf2536b2d2016a
leticia-rsm <leticia.mendes@sptech.school>
2025-06-06 11:04:41 -0300

    Alterações na captura de dados externos

--- web-crawler/lambda_function.py
+++ web-crawler/lambda_function.py
@@ -6,27 +6,24 @@ import tempfile
 
 import os
 
-import json
-
 import boto3
 
 from meteostat import Point, Daily
 
 from datetime import datetime
 
-from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-import csv
+import pandas as pd
 
 def lambda_handler(event, context):
-
-    mes_atual = datetime.now().month
-    ano_atual = datetime.now().year
-    data = f"{ano_atual}{mes_atual:02d}"
-
     try:
-        start = datetime(2020, 1, 1)
-        end = datetime(2020, 12, 31)
+        hoje = datetime.now()
+
+        tres_meses_atras = hoje - relativedelta(months=3)
+
+        start = tres_meses_atras
+        end = hoje
 
         location = Point(-23.55, -46.63)
 
@@ -34,19 +31,26 @@ def lambda_handler(event, context):
         data = data.fetch()
 
         data = data.reset_index()
-
         dadosFiltrados = data[['time', 'tavg', 'tmin', 'tmax', 'prcp', 'wspd']]
 
-        dadosFiltrados.to_csv('/tmp/clima.csv', index=False, encoding='utf-8-sig')
+        dadosFiltrados = dadosFiltrados.fillna(0.0)
 
-        nome_arquivo = f"/tmp/clima{data}.csv"
+        nome_arquivo = '/tmp/clima.csv'
+        dadosFiltrados.to_csv(nome_arquivo, index=False, encoding='utf-8-sig')
 
-        nome_arquivoDestino = f"clima/clima{data}.csv"
+        nome_arquivo_destino = f"clima/clima_{hoje.strftime('%Y-%m-%d')}.csv"
+
         s3 = boto3.client('s3')
-        s3.upload_file(nome_arquivo, 'bucket', nome_arquivoDestino)
+        s3.upload_file(nome_arquivo, 'bucket', nome_arquivo_destino)
 
-    except requests.exceptions.RequestException as e:
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Arquivo enviado com sucesso!')
+        }
 
-        print(f"Erro na requisição: {e}")
-
-        return None
+    except Exception as e:
+        print(f"Erro: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Erro: {e}")
+        }
